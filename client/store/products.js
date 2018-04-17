362101a04ee0937b3c1eabc7d54329be69b8589b
chrisstpehenmiller <csmiller4567@gmail.com>
2018-04-17 14:56:00 -0500

    all products

---
+++ client/store/products.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_PRODUCTS = 'GET_PRODUCTS'
+
+/**
+ * INITIAL STATE
+ */
+const defaultProducts = []
+
+/**
+ * ACTION CREATORS
+ */
+const getProducts = products => ({type: GET_PRODUCTS, products})
+
+/**
+ * THUNK CREATORS
+ */
+export const me = () =>
+  dispatch =>
+    axios.get('/auth/me')
+      .then(res =>
+        dispatch(getUser(res.data || defaultUser)))
+      .catch(err => console.log(err))
+
+/**
+ * REDUCER
+ */
+export default function (state = defaultProducts, action) {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return action.procuts
+    default:
+      return state
+  }
+}
