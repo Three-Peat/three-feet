b5a28b66b2c68c77b19874f0f755827ca49fbca4
chrisstpehenmiller <csmiller4567@gmail.com>
2018-04-18 16:38:41 -0500

    fixed seed-file

--- seed-feet.js
+++ seed-feet.js
@@ -10,7 +10,7 @@ const data = {
   // orderItem: [],
   product: [],
   productCategory: [],
-  // review: [],
+  review: [],
   user: [],
 }
 
@@ -36,10 +36,10 @@ for (let i = 0; i < 10; i++) {
   const product = {
     name: faker.commerce.productName(),
     description: faker.company.bs(),
-    price: faker.random.number({min: 99, max: 50000}),
-    inventory: faker.random.number({min: 0, max: 50}),
+    price: faker.random.number({ min: 99, max: 50000 }),
+    inventory: faker.random.number({ min: 0, max: 50 }),
     brand: faker.company.companyName(),
-    size: faker.random.number({min: 6, max: 15}),
+    size: faker.random.number({ min: 6, max: 15 }),
     color: faker.commerce.color(),
     photoUrl: `http://icons.iconarchive.com/icons/iconsmind/outline/256/Running-Shoes-icon.png`
   }
@@ -51,17 +51,22 @@ for (let i = 0; i < 10; i++) {
   const category = {
     name: faker.commerce.productMaterial(),
     description: faker.company.bs(),
-    productId: faker.random.number({min:1, max: 10})
+    productId: faker.random.number({ min: 1, max: 10 })
   }
   data.category.push(category)
 }
 
-for (let i = 0; i < 15; i++) {
+const checker = []
+for (let i = 0; i < 50; i++) {
   const productCategory = {
-    productId: faker.random.number({min: 1, max: 10}),
-    categoryId: faker.random.number({min: 1, max: 10}),
+    productId: faker.random.number({ min: 1, max: 10 }),
+    categoryId: faker.random.number({ min: 1, max: 10 }),
   }
-  data.productCategory.push(productCategory)
+  const stringId = `${productCategory.productId}${productCategory.categoryId}`
+  if (!checker.includes(stringId)) {
+    data.productCategory.push(productCategory)
+    checker.push(stringId)
+  }
 }
 
 // for (let i = 0; i < 10; i++) {
@@ -82,35 +87,35 @@ db.sync({ force: true })
   .then(function () {
     console.log("Dropped old data, now inserting data.");
     return Promise.all([
-      // Promise.map(data[`address`], function (entry) {
+      // Promise.map(data.address`], function (entry) {
       //   return db.model(`address`)
       //     .create(entry)
       // }),
-      // Promise.map(data[`order`], function (entry) {
+      // Promise.map(data.order`], function (entry) {
       //   return db.model(`order`)
       //     .create(entry)
       // }),
-      // Promise.map(data[`orderentry`], function (entry) {
+      // Promise.map(data.orderentry`], function (entry) {
       //   return db.model(`orderentry`)
       //     .create(entry)
       // }),
-      Promise.map(data[`product`], function (entry) {
+      Promise.map(data.product, function (entry) {
         return db.model(`product`)
           .create(entry)
       }),
-      Promise.map(data[`category`], function (entry) {
+      Promise.map(data.category, function (entry) {
         return db.model(`category`)
           .create(entry)
       }),
-      Promise.map(data[`productCategory`], function (entry) {
+      Promise.map(data.productCategory, function (entry) {
         return db.model(`productCategory`)
           .create(entry)
       }),
-      // Promise.map(data[`review`], function (entry) {
+      // Promise.map(data.review, function (entry) {
       //   return db.model(`review`)
       //     .create(entry)
       // }),
-      Promise.map(data[`user`], function (entry) {
+      Promise.map(data.user, function (entry) {
         return db.model(`user`)
           .create(entry)
       })
