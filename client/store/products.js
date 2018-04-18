4cd1763e480391f2909dfb037d280da87361dc06
nangelos <nicksangelos@gmail.com>
2018-04-17 20:49:35 -0500

    first product detail commit

--- client/store/products.js
+++ client/store/products.js
@@ -1,39 +1,48 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
 
 /**
  * ACTION TYPES
  */
-const GET_PRODUCTS = 'GET_PRODUCTS'
+const GET_PRODUCTS = 'GET_PRODUCTS';
+const GET_PRODUCT = 'GET_PRODUCT';
 
 /**
  * INITIAL STATE
  */
-const defaultProducts = []
+const defaultProducts = [];
 
 /**
  * ACTION CREATORS
  */
-const getProducts = products => ({type: GET_PRODUCTS, products})
+const getProducts = products => ({ type: GET_PRODUCTS, products });
+const getProduct = product => ({ type: GET_PRODUCT, product });
 
 /**
  * THUNK CREATORS
  */
-export const allProducts = () =>
-  dispatch =>
-    axios.get('/api/products')
-      .then(res =>
-        dispatch(getProducts(res.data || defaultProducts)))
-      .catch(err => console.log(err))
+export const allProducts = () => dispatch =>
+  axios
+    .get('/api/products')
+    .then(res => dispatch(getProducts(res.data || defaultProducts)))
+    .catch(err => console.log(err));
+
+export const oneProduct = id => dispatch =>
+  axios
+    .get(`/api/products/${id}`)
+    .then(res => dispatch(getProduct(res.data || defaultProducts)))
+    .catch(err => console.error(err));
 
 /**
  * REDUCER
  */
-export default function (state = defaultProducts, action) {
+export default function(state = defaultProducts, action) {
   switch (action.type) {
     case GET_PRODUCTS:
-      return action.products
+      return action.products;
+    case GET_PRODUCT:
+      return action.product;
     default:
-      return state
+      return state;
   }
 }
