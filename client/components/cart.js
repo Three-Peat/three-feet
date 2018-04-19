a2778b4018bfa63aa9f3246260cfa2e4d7624581
joeydeeds <joey.deeds@gmail.com>
2018-04-19 13:07:56 -0500

    forgot to save a file

--- client/components/cart.js
+++ client/components/cart.js
@@ -3,20 +3,6 @@ import { connect } from 'react-redux';
 
 export class Cart extends Component {
 
-<<<<<<< HEAD
-=======
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '',
-    };
-  }
-
-  componentDidMount = () => {
-    const { getCart } = this.props;
-    // getCart()
-  };
->>>>>>> aa6aeda62b1a6ec370c2057b558d0d5ec0adc2ef
 
   render() {
 
