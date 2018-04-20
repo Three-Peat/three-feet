bc21d59993072903e45e84661c0130da79a4863a
joeydeeds <joey.deeds@gmail.com>
2018-04-19 21:57:11 -0500

    adding to cart kinda works

--- client/components/cart.js
+++ client/components/cart.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Cart extends Component {
-
-
   render() {
-
+    const { cart } = this.props
+    // right now this is a cart id, not product ID
     return (
       <div>
         <p>My Cart</p>
+        {cart.map(product => {
+          return <p key={product.id}>{product.id}</p>
+        })}
       </div>
     );
   }
 }
+const mapState = state => {
+  const { cart } = state;
+  return { cart };
+};
 
-export default Cart
+const mapDispatch = dispatch => {
+  return {
+    // Going to need this eventually
+    // removeFromCart: product => {
+    //   dispatch(removeFromCart(product));
+    // },
+  };
+};
+
+export default connect(mapState, mapDispatch)(Cart);
