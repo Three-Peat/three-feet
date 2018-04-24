71214b445cc0c3313bf064381fac1c54195bb9a3
nangelos <nicksangelos@gmail.com>
2018-04-23 22:47:31 -0500

    working on address-input

---
+++ client/components/address-input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddressInput = () => {
+  return (
+    <div>
+      <input placeholder="Name" />
+      <input placeholder="Street" />
+      <input placeholder="City" />
+      <input placeholder="State" />
+      <input placeholder="Zip" />
+    </div>
+  );
+};
+
+export default AddressInput;
